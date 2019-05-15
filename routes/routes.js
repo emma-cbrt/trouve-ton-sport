69ad645c76e5773fea1fc75962d2798f15ff7016
emma_cbrt <44927969+emma-cbrt@users.noreply.github.com>
2019-05-16 00:24:39 +0200

    mises à jour

--- routes/routes.js
+++ routes/routes.js
@@ -17,19 +17,19 @@ router.get('/', function(req,res){
 
 //Routes pour ClubSport 
 .get('/ClubSport', function(req,res){
-	res.render('ClubSport.ejs');
+	res.render('clubSport.ejs');
 })
 
 //TODO get avec paramètre
 // .get('/ClubSport', function(req,res){
-// 	res.render('ClubSport.ejs');
+// 	res.render('clubSport.ejs');
 // });)
 
 	/* Page Evenement */
 
 //route pour Evenement
 .get('/Evenement', function(req,res){
-	res.render('Evenement.ejs');
+	res.render('evenement.ejs');
 })
 
 //TODO get avec paramètre
@@ -40,26 +40,115 @@ router.get('/', function(req,res){
 
 //seulement si client connecté
 .get('/Evenement/NewEvenement', function(req,res){
-	res.render('NewEvenement.ejs');
+	res.render('newEvenement.ejs');
 })
 
 .post('/Evenement/NewEvenement', function(req,res){
-	//envoyer à la base de donnée
-	res.redirect('Evenement.ejs')
+	//ajouter événement à la base de donnée
+	res.redirect('evenement.ejs')
 })
 
-	/* Page Utilisateur */
+	/* Page Partenaires */
+
+//route pour Partenaire
+.get('/Partenaire', function(req,res){
+	//si co 
+	res.render('partenaires.ejs')
+	//sinon
+	res.render('connexion.ejs')
+})
+
+//TODO
+//faire get avec des paramètres de villes et sport
+
+	/* Page Inscription/Connection */
 
 //inscription (renvoit le formulaire d'inscription)
 .get('/Inscription', function(req,res){
 	res.render('Inscription.ejs')
-});
+})
 
 //enregitre données utilisateurs dans base de données
-// .post()
+.post('/Inscription', function(req,res){
+	//ajouter utilisateur à BD
+	res.redirect('index.ejs')
+})
+
+// page de connexion
+.get('/Connexion', function(req,res){
+	//ajouter utilisateur à BD
+	res.redirect('connexion.ejs')
+})
+
+// entrée du mail et mdp (connexion)
+.put('/Connexion', function(req,res){
+	//si ok
+	res.render('index.ejs')
+	//sinon
+	res.render('connexion.ejs')
+})
+
+//deconnexion
+.delete('/Connexion', function(req,res){
+	res.render('index.ejs')
+})
 
 
 
+	/* Page Utilisateur */
+
+//Page mon compte
+.get('/Comptes/:iduser', function(req,res){
+	res.render('monCompte.ejs')
+})
+
+.delete('/Comptes/:iduser', function(req,res){
+	//enlever user BD
+	res.redirect('index.ejs')
+})
+
+.put('/Comptes/:iduser', function(req,res){
+	//renvoyer entité entière à BD pour mise à jour
+	res.redirect('monCompte.ejs')
+})
+
+//Page mes sports et mes évenement et mes partenaires
+	//mes sports
+.get('/Comptes/:iduser/mesSports', function(req,res){
+	res.render('mesSports.ejs')
+})
+//Ajout d'un sport de l'utilisateur
+.put('/Comptes/:iduser/mesSports', function(req,res){
+	res.render('mesSports.ejs')
+})
+//Suppression d'un sport de l'utilisateur
+.delete('/Comptes/:iduser/mesSports', function(req,res){
+	res.render('mesSports.ejs')
+})
+	//mes evenements
+.get('/Comptes/:iduser/mesEvenements', function(req,res){
+	res.render('mesEvenements.ejs')
+})
+//Ajout d'un evenement créé par l'utilisateur
+.put('/Comptes/:iduser/mesEvenements', function(req,res){
+	res.render('mesEvenements.ejs')
+})
+//Suppression d'un evenement créé par l'utilisateur
+.delete('/Comptes/:iduser/mesEvenements', function(req,res){
+	res.render('mesEvenements.ejs')
+})
+	//mes partenaires
+.get('/Comptes/:iduser/mesPartenaires', function(req,res){
+	res.render('mesPartenaires.ejs')
+})
+//Ajout d'un partenaire pour l'utilisateur
+.put('/Comptes/:iduser/mesParetnaires', function(req,res){
+	res.render('mesPartenaires.ejs')
+})
+//Suppression d'un partenaire de l'utilisateur
+.delete('/Comptes/:iduser/mesEvenements', function(req,res){
+	res.render('mesPartenaires.ejs')
+});
 
 
 //rendre le module accessible aux autres pages
--- server.js
+++ server.js
@@ -1,32 +1,48 @@
-//charge le module d'express(sorte de framework).
-//La variable express sert à utiliser fonctionnalités d'Express.
-var express = require('express');
-//charle le module mysql 
-var mysql = require('mysql');
-// Charge le middleware de sessions
-var cookieParser = require('cookie-parser');
-// Charge le middleware de gestion des paramètres
-var bodyParser = require('body-parser'); 
-var path = require('path');
-var logger = require('morgan');
-var Routes = require('./routes/index.js');
+/* Chargement des librairies nécessaire au bon fonctionnement de l'app */
 
+	//charge le module d'express(sorte de framework).
+	//La variable express sert à utiliser fonctionnalités d'Express.
+	var express = require('express');
+	// Charge le middleware de mysql 
+	var mysql = require('mysql');
+	//charge le middleware de cookie-parser afin d'utiliser les cookies avec express
+	var cookieParser = require('cookie-parser');
+	//module parses the JSON, buffer, string and URL 
+	//encoded data submitted using HTTP POST request. 
+	var bodyParser = require('body-parser'); 
+	//The Path module provides a way of working with directories and file paths
+	var path = require('path');
+	//s and basically used as a logger. It can be used 
+	//with node js' winston package to consolidate HTTP request data logs with other i
+	var morgan = require('morgan');
+	//notre module routes
+	var Routes = require('./routes/routes.js');
+
+// Create a new Express Instance (une application)
 var app = express();
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: false }));
 
-//charge les routes
-app.use('/', Routes); 
+//donne accès à l'application pour utiliser le dossier static public
+//qui contient les images, css etc..
+app.use(express.static(path.join(__dirname, 'public')))
+
+//set the view engine to ejs
+.set('view engine', 'ejs')
+
+//précise à au serveur utilise body parser configuré
+// pour l'encodage classique
+.use(bodyParser.urlencoded({ extended: false }))
+
+//notre serveur se sert de notre module routes
+.use('/', Routes)
 
 //Route 404
-app.use('*', (req, res) => {
+.use('*', (req, res) => {
     res.status(404).send('Error 404');
-});
+})
 
 
-app.listen(3001);
+.listen(8080);
 
 
 
